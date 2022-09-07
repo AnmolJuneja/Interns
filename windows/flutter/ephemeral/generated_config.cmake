f61b7e54f82b747331a0e64accdb0b2c92ef0141
sagardawra <111624190+sagardawra@users.noreply.github.com>
2022-09-07 22:37:17 +0530

    Reelpro-first-cut
    Reelpro-first-cut

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "G:\\taxi-booking" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\src\\flutter"
+  "PROJECT_DIR=G:\\taxi-booking"
+  "FLUTTER_ROOT=F:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=G:\\taxi-booking\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=G:\\taxi-booking"
+  "FLUTTER_TARGET=G:\\taxi-booking\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=G:\\taxi-booking\\.dart_tool\\package_config.json"
+)
