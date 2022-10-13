cbbca557976e26d1f467cc05f29548fd5a2c58d0
ajaychayal <ajay.chayal@yatilabs.com>
2022-10-13 18:28:24 +0530

    ajay-first-commit

---
+++ src/pages/Gallery.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="first-work four">
+      <h1>Hello Banner images Four</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore in
+        corrupti ad fuga iure ab explicabo, tempora minima quas vel esse unde
+        quod praesentium. Pariatur accusamus aliquid ut maxime alias?
+      </p>
+    </section>
+  );
+};
+
+export default About;
